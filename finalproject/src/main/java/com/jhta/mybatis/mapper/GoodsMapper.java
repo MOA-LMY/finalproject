f57b0ac2640520085faf2a3898b466df959831e2
상현 <@DESKTOP-LEP41D1>
2021-08-02 15:05:43 +0900

    Goodsmapper 업데이트

--- finalproject/src/main/java/com/jhta/mybatis/mapper/GoodsMapper.java
+++ finalproject/src/main/java/com/jhta/mybatis/mapper/GoodsMapper.java
@@ -9,5 +9,10 @@ import com.jhta.finalproject.vo.GoodsVo;
 public interface GoodsMapper {
 	
 	int insert(GoodsVo vo);
-	List<GoodsVo> list(); 
+	List<GoodsVo> list();
+	int delete(int g_num);
+	GoodsVo find(int g_num);
+	int update(GoodsVo vo);
+	
+	
 }
