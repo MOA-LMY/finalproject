79f26b37225a6102f59ed2a91c8da287974b341d
leewontae <whsslfp111@naver.com>
2021-08-09 17:46:52 +0900

    test

--- finalproject/src/main/java/com/jhta/finalproject/controller/shop/Goods_listController.java
+++ finalproject/src/main/java/com/jhta/finalproject/controller/shop/Goods_listController.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jhta.finalproject.service.GcsService;
 import com.jhta.finalproject.service.GoodsService;
 import com.jhta.finalproject.service.GoodscategoryService;
+import com.jhta.finalproject.vo.Gcs2Vo;
 import com.jhta.finalproject.vo.GoodsCategoryVo;
 import com.jhta.finalproject.vo.GoodsVo;
 
@@ -18,17 +20,21 @@ public class Goods_listController {
 	
 	@Autowired GoodsService goodsservice;
 	@Autowired GoodscategoryService goodscategoryservice;
+	@Autowired GcsService gcsservice;
 	@RequestMapping("/shop/goods_detail")
 	public String goods_detail(int gc_num, Model model) {
 		
 		System.out.println(gc_num+ " 상품 디데일 페이지 이동");
 		GoodsVo vo = goodsservice.find(gc_num);
 		List<GoodsVo> goodslist= goodsservice.goodlist(gc_num);
+		List<Gcs2Vo> gcslist= gcsservice.gcslist(gc_num);
 		
 		List<GoodsCategoryVo> goodscategorylist = goodscategoryservice.list();
 		model.addAttribute("goodslist",goodslist);
+		model.addAttribute("gcslist",gcslist);
 		model.addAttribute("goodscategorylist",goodscategorylist);
 		model.addAttribute("vo", vo);
 		return "shop/goods_list";
+		
 	}
 }
