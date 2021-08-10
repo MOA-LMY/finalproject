acfd91f8d59a74535e9feb288edebe64287a3db8
owner <owner@DESKTOP-JUEOOL3>
2021-08-10 16:45:11 +0900

    연식 테스트

---
+++ finalproject/src/main/java/com/jhta/mybatis/mapper/PartnersMapper.java
@@ -0,0 +1,21 @@
+package com.jhta.mybatis.mapper;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.jhta.finalproject.vo.PartnersVo;
+
+public interface PartnersMapper {
+	int insert(PartnersVo vo);
+
+ int update(PartnersVo vo) ;
+ int delete(String id) ;
+ List<PartnersVo> selectAll();
+
+ PartnersVo select(String id);
+
+ PartnersVo isPartners(HashMap<String, String> map);
+
+ PartnersVo idCheck(String id);
+
+}
