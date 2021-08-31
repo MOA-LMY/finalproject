c8fd3528f67e4c4bf733c7f23c2b7a747e93664f
Chan <Chan@59.5.70.133>
2021-09-01 01:53:08 +0900

    aa

--- finalproject/src/main/java/com/jhta/finalproject/sec/LoginSuccessHandler.java
+++ finalproject/src/main/java/com/jhta/finalproject/sec/LoginSuccessHandler.java
@@ -55,6 +55,20 @@ public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
 				
 				response.sendRedirect(request.getContextPath()+"/");
 			}
+		}else if(roleName.contains("ROLE_ADMIN")) {
+			//스프링시큐리티가 요청을 가로챘을시 요청에 대한 정보를 갖는 캐시객체 얻어오기
+			RequestCache requestCache = new HttpSessionRequestCache();
+			//저장된 사용자 요청정보를 갖는 객체 얻어오기
+			SavedRequest savedRequest = requestCache.getRequest(request, response);
+			//사용자가 이전에 요청한 url가져오기
+			try {
+				String url=savedRequest.getRedirectUrl();	
+				System.out.println(url);
+				response.sendRedirect(url);
+			}catch(Exception e) {
+				
+				response.sendRedirect(request.getContextPath()+"/");
+			}
 		}
 		else {
 			response.sendRedirect(request.getContextPath()+"/");
